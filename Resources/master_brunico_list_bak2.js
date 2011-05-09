5412a5a3b5a9be745ade09112c52bc26dcdef904
Edward Ting <e46tiag@gmail.com>
2011-05-09 12:27:35 -0400

    updated to store into db

--- Resources/master_brunico_list_bak2.js
+++ Resources/master_brunico_list_bak2.js
@@ -11,32 +11,17 @@ xhr.onload = function()
 {
 	try
 	{
-		
-		var db = Ti.Database.open('feedDB')
-		db.execute('CREATE TABLE IF NOT EXISTS tblfeed (title VARCHAR(16) NOT NULL, url VARCHAR(16) NOT NULL)');
-		
 		var doc = this.responseXML.documentElement;
 		var items = doc.getElementsByTagName("item");
 		var x = 0;
 		var doctitle = doc.evaluate("//channel/title/text()").item(0).nodeValue;
 		for (var c=0;c<items.length;c++)
 		{
-			var item = items.item(c);			
+			var item = items.item(c);
+			
 			var title = item.getElementsByTagName("title").item(0).text;
-			var url = item.getElementsByTagName("link").item(0).text;
-			db.execute('INSERT INTO tblfeed (title,url) VALUES (?,?)',title,url);
-		}
-		
-		var feedRS = db.execute('SELECT title,url FROM tblfeed');
-		
-		var x = 0;
-		while (feedRS.isValidRow())
-		{
-		  var title = feedRS.fieldByName('title');
-		  var url = feedRS.fieldByName('url');
-
-		  var row = Ti.UI.createTableViewRow({height:50});
-		  var label = Ti.UI.createLabel({
+			var row = Ti.UI.createTableViewRow({height:50});
+			var label = Ti.UI.createLabel({
 					text:title,
 					left:72,
 					top:5,
@@ -45,13 +30,10 @@ xhr.onload = function()
 				});
 			row.add(label);
 			data[x++] = row;
-			row.url = url;
-		  
-		  Ti.API.info(title + ' ' + url + ' ');
-		  feedRS.next();
+			row.url = item.getElementsByTagName("link").item(0).text;	
+			
+			
 		}
-		feedRS.close();
-		
 		var tableview = Titanium.UI.createTableView({data:data});
 		Titanium.UI.currentWindow.add(tableview);
 		tableview.addEventListener('click',tableClick);			
@@ -62,9 +44,8 @@ xhr.onload = function()
 			"title": e.row.url 
 		}
 		Ti.App.fireEvent('app:rowClicked', evtData);
-			ad
-		}
-		
+ad
+	}
 		
 		
 	}
